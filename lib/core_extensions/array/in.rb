cbef901078751ab50088d71fd67e5bd6b9cc8adc
Steve Downey <steve.downtown@gmail.com>
2010-07-24 11:54:56 -0700

    trying out yarddoc

--- lib/core_extensions/array/in.rb
+++ lib/core_extensions/array/in.rb
@@ -2,6 +2,9 @@ class Array
 
   # Returns SQL IN clause from Array for help building SQL statements:
   #
+  # @param [Hash] options
+  # @returns [String] parenthesized, comma-separated list of values (quoted if they are not numeric)
+  #
   #   ids = %w(a b c)
   #   "select * from foo where id in #{ids.in}"  #=>  "select * from foo where id in ('a','b','c')"
   #
